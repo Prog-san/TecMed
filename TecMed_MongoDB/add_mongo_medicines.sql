44f13989abef82d46f9b772bae85ff43e3b5345a
DiegoS-R <u202021367@upc.edu.pe>
2023-07-01 12:06:30 -0500

    Feat: Ubicaciones Añadidas & Correccion de la coleccion Medicines

---
+++ TecMed_MongoDB/add_mongo_medicines.sql
@@ -0,0 +1,325 @@
+db.Medicines.insertOne({
+  cod_medicine: 1,
+  med_name: "Paracetamol",
+  quantity: 50,
+  details: {
+    med_measure: "mg",
+    med_weight: 500,
+    med_price: 3.99,
+    expiration: new Date(2024, 1, 15),
+    production: new Date(2023, 8, 20)
+  }
+});
+
+db.Medicines.insertOne({
+  cod_medicine: 2,
+  med_name: "Ibuprofeno",
+  quantity: 30,
+  details: {
+    med_measure: "mg",
+    med_weight: 600,
+    med_price: 4.49,
+    expiration: new Date(2023, 4, 15),
+    production: new Date(2022, 11, 5)
+  }
+});
+
+db.Medicines.insertOne({
+  cod_medicine: 3,
+  med_name: "Amoxicilina",
+  quantity: 80,
+  details: {
+    med_measure: "mg",
+    med_weight: 250,
+    med_price: 8.99,
+    expiration: new Date(2024, 3, 15),
+    production: new Date(2022, 9, 20)
+  }
+});
+
+db.Medicines.insertOne({
+  cod_medicine: 4,
+  med_name: "Loratadina",
+  quantity: 30,
+  details: {
+    med_measure: "mg",
+    med_weight: 10,
+    med_price: 3.49,
+    expiration: new Date(2023, 10, 30),
+    production: new Date(2022, 8, 10)
+  }
+});
+
+db.Medicines.insertOne({
+  cod_medicine: 5,
+  med_name: "Omeprazol",
+  quantity: 60,
+  details: {
+    med_measure: "mg",
+    med_weight: 20,
+    med_price: 7.99,
+    expiration: new Date(2023, 7, 31),
+    production: new Date(2022, 7, 25)
+  }
+});
+
+db.Medicines.insertOne({
+  cod_medicine: 6,
+  med_name: "Cetirizina",
+  quantity: 40,
+  details: {
+    med_measure: "mg",
+    med_weight: 5,
+    med_price: 4.29,
+    expiration: new Date(2024, 1, 28),
+    production: new Date(2022, 6, 15)
+  }
+});
+
+db.Medicines.insertOne({
+  cod_medicine: 7,
+  med_name: "Dipirona",
+  quantity: 200,
+  details: {
+    med_measure: "mg",
+    med_weight: 1000,
+    med_price: 6.49,
+    expiration: new Date(2024, 2, 15),
+    production: new Date(2022, 5, 30)
+  }
+});
+
+db.Medicines.insertOne({
+  cod_medicine: 8,
+  med_name: "Aspirina",
+  quantity: 150,
+  details: {
+    med_measure: "mg",
+    med_weight: 500,
+    med_price: 3.99,
+    expiration: new Date(2023, 11, 30),
+    production: new Date(2022, 4, 20)
+  }
+});
+
+db.Medicines.insertOne({
+  cod_medicine: 9,
+  med_name: "Prednisona",
+  quantity: 50,
+  details: {
+    med_measure: "mg",
+    med_weight: 10,
+    med_price: 9.99,
+    expiration: new Date(2023, 4, 30),
+    production: new Date(2022, 3, 10)
+  }
+});
+
+db.Medicines.insertOne({
+  cod_medicine: 10,
+  med_name: "Ciprofloxacina",
+  quantity: 30,
+  details: {
+    med_measure: "mg",
+    med_weight: 250,
+    med_price: 5.99,
+    expiration: new Date(2024, 3, 30),
+    production: new Date(2023, 2, 5)
+  }
+});
+
+db.Medicines.insertOne({
+  cod_medicine: 11,
+  med_name: "Losartan",
+  quantity: 100,
+  details: {
+    med_measure: "mg",
+    med_weight: 50,
+    med_price: 11.49,
+    expiration: new Date(2024, 5, 30),
+    production: new Date(2023, 1, 15)
+  }
+});
+
+db.Medicines.insertOne({
+  cod_medicine: 12,
+  med_name: "Atorvastatina",
+  quantity: 80,
+  details: {
+    med_measure: "mg",
+    med_weight: 20,
+    med_price: 8.99,
+    expiration: new Date(2023, 8, 31),
+    production: new Date(2023, 1, 5)
+  }
+});
+
+db.Medicines.insertOne({
+  cod_medicine: 13,
+  med_name: "Metformina",
+  quantity: 120,
+  details: {
+    med_measure: "mg",
+    med_weight: 850,
+    med_price: 6.75,
+    expiration: new Date(2024, 7, 31),
+    production: new Date(2023, 11, 20)
+  }
+});
+
+db.Medicines.insertOne({
+  cod_medicine: 14,
+  med_name: "Escitalopram",
+  quantity: 50,
+  details: {
+    med_measure: "mg",
+    med_weight: 10,
+    med_price: 14.85,
+    expiration: new Date(2024, 10, 30),
+    production: new Date(2023, 10, 5)
+  }
+});
+
+db.Medicines.insertOne({
+  cod_medicine: 15,
+  med_name: "Warfarina",
+  quantity: 40,
+  details: {
+    med_measure: "mg",
+    med_weight: 5,
+    med_price: 10.20,
+    expiration: new Date(2025, 1, 31),
+    production: new Date(2023, 9, 15)
+  }
+});
+
+db.Medicines.insertOne({
+  cod_medicine: 16,
+  med_name: "Amoxicilina",
+  quantity: 60,
+  details: {
+    med_measure: "mg",
+    med_weight: 500,
+    med_price: 7.30,
+    expiration: new Date(2024, 11, 30),
+    production: new Date(2023, 8, 10)
+  }
+});
+
+db.Medicines.insertOne({
+  cod_medicine: 17,
+  med_name: "Simvastatina",
+  quantity: 30,
+  details: {
+    med_measure: "mg",
+    med_weight: 20,
+    med_price: 9.99,
+    expiration: new Date(2024, 3, 30),
+    production: new Date(2023, 7, 25)
+  }
+});
+
+db.Medicines.insertOne({
+  cod_medicine: 18,
+  med_name: "Clonazepam",
+  quantity: 40,
+  details: {
+    med_measure: "mg",
+    med_weight: 2,
+    med_price: 12.45,
+    expiration: new Date(2023, 6, 30),
+    production: new Date(2023, 6, 1)
+  }
+});
+
+db.Medicines.insertOne({
+  cod_medicine: 19,
+  med_name: "Gabapentina",
+  quantity: 90,
+  details: {
+    med_measure: "mg",
+    med_weight: 300,
+    med_price: 11.80,
+    expiration: new Date(2024, 9, 30),
+    production: new Date(2023, 4, 15)
+  }
+});
+
+db.Medicines.insertOne({
+  cod_medicine: 20,
+  med_name: "Omeprazol",
+  quantity: 70,
+  details: {
+    med_measure: "mg",
+    med_weight: 20,
+    med_price: 9.25,
+    expiration: new Date(2023, 7, 31),
+    production: new Date(2023, 2, 20)
+  }
+});
+
+db.Medicines.insertOne({
+  cod_medicine: 21,
+  med_name: "Bromazepam",
+  quantity: 50,
+  details: {
+    med_measure: "mg",
+    med_weight: 6,
+    med_price: 15.70,
+    expiration: new Date(2024, 2, 28),
+    production: new Date(2023, 1, 5)
+  }
+});
+
+db.Medicines.insertOne({
+  cod_medicine: 22,
+  med_name: "Sertralina",
+  quantity: 80,
+  details: {
+    med_measure: "mg",
+    med_weight: 50,
+    med_price: 11.99,
+    expiration: new Date(2024, 8, 30),
+    production: new Date(2023, 11, 10)
+  }
+});
+
+db.Medicines.insertOne({
+  cod_medicine: 23,
+  med_name: "Paracetamol",
+  quantity: 150,
+  details: {
+    med_measure: "mg",
+    med_weight: 500,
+    med_price: 5.75,
+    expiration: new Date(2023, 11, 30),
+    production: new Date(2023, 3, 22)
+  }
+});
+
+db.Medicines.insertOne({
+  cod_medicine: 24,
+  med_name: "Furosemida",
+  quantity: 100,
+  details: {
+    med_measure: "mg",
+    med_weight: 40,
+    med_price: 8.25,
+    expiration: new Date(2023, 9, 30),
+    production: new Date(2023, 8, 1)
+  }
+});
+
+db.Medicines.insertOne({
+  cod_medicine: 25,
+  med_name: "Risperidona",
+  quantity: 60,
+  details: {
+    med_measure: "mg",
+    med_weight: 3,
+    med_price: 14.45,
+    expiration: new Date(2024, 5, 30),
+    production: new Date(2023, 3, 10)
+  }
+});
+
